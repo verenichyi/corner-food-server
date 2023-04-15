3f325232c615a3ecd95e32c4dcf3223601be6021
verenichyi <verenichyi@gmail.com>
2023-04-15 16:38:00 +0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({ origin: process.env.CLIENT_URI });
   app.useGlobalPipes(new ValidationPipe());
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
