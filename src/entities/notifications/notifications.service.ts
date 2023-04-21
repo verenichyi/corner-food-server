0087d16ad3e67262edc116ceb88540c5b77c5dc5
verenichyi <verenichyi@gmail.com>
2023-04-21 20:55:16 +0300

    chore: remove redundant module

--- src/entities/notifications/notifications.service.ts
+++ src/entities/notifications/notifications.service.ts
@@ -1,50 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { OrderService } from '../order/order.service';
 import { Notification } from './entities/notification.entity';
-import * as moment from 'moment';
-
-const enum StatusList {
-  Processing = 'processing',
-  Preparing = 'preparing',
-  Delivering = 'delivering',
-}
-
-const getOrderStatus = (deliveryTime: number, orderCreatedAt: Date): string => {
-  const deliveryTimeInSeconds = deliveryTime * 60;
-  const processingTimeInSeconds = (deliveryTimeInSeconds * 10) / 100;
-  const preparingTimeInSeconds = (deliveryTimeInSeconds * 50) / 100;
-  const deliveringTimeInSeconds = (deliveryTimeInSeconds * 40) / 100;
-
-  const processingDueDate = moment(orderCreatedAt).add(
-    processingTimeInSeconds,
-    's',
-  );
-  const preparingDueDate = moment(orderCreatedAt)
-    .add(processingTimeInSeconds, 's')
-    .add(preparingTimeInSeconds, 's');
-  const deliveringDueDate = moment(orderCreatedAt)
-    .add(processingTimeInSeconds, 's')
-    .add(preparingTimeInSeconds, 's')
-    .add(deliveringTimeInSeconds, 's');
-
-  const isProcessingTime = moment().isBefore(processingDueDate);
-  const isPreparingTime = moment().isBetween(
-    processingDueDate,
-    preparingDueDate,
-  );
-  const isDeliveringTime = moment().isBetween(
-    preparingDueDate,
-    deliveringDueDate,
-  );
-
-  if (isProcessingTime) {
-    return StatusList.Processing;
-  } else if (isPreparingTime) {
-    return StatusList.Preparing;
-  } else if (isDeliveringTime) {
-    return StatusList.Delivering;
-  }
-};
+import { getOrderStatus } from '../../utils/getOrderStatus';
 
 @Injectable()
 export class NotificationsService {
--- src/entities/order/order.controller.ts
+++ src/entities/order/order.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { JwtAuthGuard } from '../../guards/jwt-auth.guard';
@@ -6,10 +6,15 @@ import { JwtAuthGuard } from '../../guards/jwt-auth.guard';
 @UseGuards(JwtAuthGuard)
 @Controller('order')
 export class OrderController {
-  constructor(private readonly paymentService: OrderService) {}
+  constructor(private readonly orderService: OrderService) {}
 
   @Post()
   async createOrder(@Body() body: CreateOrderDto) {
-    return await this.paymentService.createOrder(body);
+    return await this.orderService.createOrder(body);
+  }
+
+  @Get(':id')
+  async findUserInactiveOrders(@Param('id') id: string) {
+    return await this.orderService.findUserInactiveOrders(id);
   }
 }
